2caacb8fff3523704d5765f2f30d201a95459362
Nuwantha Sanduka <nuwanthasanduka4@gmail.com>
2026-07-14 17:31:35 +0530

    Add text to speech module

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+from core.voice import speak
+
 print("====================================")
 print("       Welcome to NEXA AI")
 print("====================================")
+
 print("System Starting...")
+
+speak("Hello Nuwantha. I am Nexa. How can I help you today?")
+
 print("Developer : Nuwantha Sanduka")
