ccd7a865f1f7f6bba3a1b50a9c6a896f590bfaa7
Nuwantha Sanduka <nuwanthasanduka4@gmail.com>
2026-08-20 20:44:16 +0530

    Add basic application voice commands

--- core/commands.py
+++ core/commands.py
@@ -1,4 +1,5 @@
 import subprocess
+import webbrowser
 
 
 def execute_command(command):
@@ -8,6 +9,21 @@ def execute_command(command):
         print("Opening Chrome...")
         subprocess.Popen("start chrome", shell=True)
         return "Opening Chrome"
+
+    elif "open notepad" in command:
+        print("Opening Notepad...")
+        subprocess.Popen("notepad.exe")
+        return "Opening Notepad"
+
+    elif "open calculator" in command:
+        print("Opening Calculator...")
+        subprocess.Popen("calc.exe")
+        return "Opening Calculator"
+
+    elif "open youtube" in command:
+        print("Opening YouTube...")
+        webbrowser.open("https://www.youtube.com")
+        return "Opening YouTube"
 
     else:
         return "Sorry, I don't know that command yet."
